6cf70a9ba1ad081a04ff8964b018e3f09574561e
SomyaGautam02 <somyagautam02@gmail.com>
2023-04-03 12:19:00 +0530

    added script.js

---
+++ wallet/user/script.js
@@ -0,0 +1,44 @@
+function disableScreen() {
+    var div= document.createElement("div");
+    div.className += "disabled_screen";
+    document.body.appendChild(div);
+}
+
+function widraw(){
+    var a=document.getElementById("current_amount").textContent;
+    var pin=prompt("Enter PIN");
+    if(pin==1111){
+        var b=prompt("Enter Amount")
+        if(b<=a){
+            var d=parseInt(a)-parseInt(b);
+            document.getElementById("current_amount").innerHTML=d;
+            confirm("Transaction Sucessfull!");
+        }
+        else{
+            alert("Enter Valid Amount!!");
+        }
+    }
+    else{
+        alert("Enter Valid Pin!!");
+    }
+}
+
+function add(){
+    var a=document.getElementById("current_amount").textContent;
+    var pin=prompt("Enter PIN");
+    if(pin==1111){
+        var b=prompt("Enter Amount")
+        if(b<1000000){
+            var d=parseInt(a)+parseInt(b);
+            document.getElementById("current_amount").innerHTML=d;
+            confirm("Transaction Sucessfull!");
+        }
+        else{
+            alert("Enter Valid Amount!!");
+        }
+    }
+    else{
+        alert("Enter Valid Pin!!");
+    }
+}
+
